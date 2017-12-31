6092e550b02e30eb887065926d90cca68bbbca3e
Christopher Dorst <chris@dorst.email>
2017-12-31 03:00:21 -0600

    Add RelativePathGetter

---
+++ Common.Functions.GetRelativePath/RelativePathGetter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.Functions.GetRelativePath
+{
+    public static class RelativePathGetter
+    {
+        public static string Relative(Uri path1, Uri path2)
+            => path1.MakeRelativeUri(path2).OriginalString;
+
+        public static string Relative(string path1, string path2)
+            => Relative(new Uri(path1), new Uri(path2));
+    }
+}
