39295a0ff7caba8b6bbef7ba3a70223ee439f5bb
Christopher Dorst <chris@dorst.email>
2018-01-01 20:05:21 -0600

    Removing first path component from RelativePathGetter result

--- Common.Functions.GetRelativePath/RelativePathGetter.cs
+++ Common.Functions.GetRelativePath/RelativePathGetter.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Linq;
+using static System.IO.Path;
 
 namespace Common.Functions.GetRelativePath
 {
     public static class RelativePathGetter
     {
         public static string Relative(Uri path1, Uri path2)
-            => path1.MakeRelativeUri(path2).OriginalString;
+            => Combine(
+                paths: path1.MakeRelativeUri(path2).OriginalString.Split('/').Skip(1).ToArray())
+                .Replace('\\', '/');
 
         public static string Relative(string path1, string path2)
             => Relative(new Uri(path1), new Uri(path2));
